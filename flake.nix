deacf98e82f7394b6b758e2bcd2f5b7f9e2b5eef
Omochice <mochice.mls.ntl@gmail.com>
2024-12-03 21:06:07 +0900

    chore: add treefmt

--- flake.nix
+++ flake.nix
@@ -16,6 +16,7 @@
       flake = false;
     };
 
+    treefmt-nix.url = "github:numtide/treefmt-nix";
     # Example of downloading icons from a non-flake source
     # font-awesome = {
     #   url = "github:FortAwesome/Font-Awesome";
@@ -23,101 +24,114 @@
     # };
   };
 
-  outputs = inputs @ {
-    nixpkgs,
-    typix,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      inherit (pkgs) lib;
+  outputs =
+    inputs@{
+      nixpkgs,
+      typix,
+      flake-utils,
+      treefmt-nix,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        # inherit (pkgs) lib;
 
-      typixLib = typix.lib.${system};
+        typixLib = typix.lib.${system};
 
-      src = typixLib.cleanTypstSource ./.;
-      # Watch a project and recompile on changes
-      watch-script = typixLib.watchTypstProject commonArgs;
+        src = typixLib.cleanTypstSource ./.;
+        # Watch a project and recompile on changes
+        watch-script = typixLib.watchTypstProject commonArgs;
 
-      typstPackagesSrc = pkgs.symlinkJoin {
-        name = "typst-packages-src";
-        paths = [
-          "${inputs.typst-packages}/packages"
-        ];
-      };
-
-      typstPackagesCache = pkgs.stdenv.mkDerivation {
-        name = "typst-packages-cache";
-        src = typstPackagesSrc;
-        dontBuild = true;
-        installPhase = ''
-          mkdir -p "$out"
-          cp -LR --reflink=auto --no-preserve=mode -t "$out" "$src"/*
-        '';
-      };
-
-      commonArgs = {
-        typstSource = "main.typ";
-
-        fontPaths = [
-          # Add paths to fonts here
-          # "${pkgs.roboto}/share/fonts/truetype"
-        ];
-
-        typstOpts = {
-          package-path = typstPackagesCache;
+        typstPackagesSrc = pkgs.symlinkJoin {
+          name = "typst-packages-src";
+          paths = [
+            "${inputs.typst-packages}/packages"
+          ];
         };
 
-        virtualPaths = [
-          # Add paths that must be locally accessible to typst here
-          # {
-          #   dest = "icons";
-          #   src = "${inputs.font-awesome}/svgs/regular";
-          # }
-        ];
-      };
-
-      # Compile a Typst project, *without* copying the result
-      # to the current directory
-      build-drv = typixLib.buildTypstProject (commonArgs
-        // {
-          inherit src;
-        });
-
-      # Compile a Typst project, and then copy the result
-      # to the current directory
-      build-script = typixLib.buildTypstProjectLocal (commonArgs
-        // {
-          inherit src;
-        });
-
-    in {
-      checks = {
-        inherit build-drv build-script watch-script;
-      };
-
-      packages.default = build-drv;
-
-      apps = rec {
-        default = watch;
-        build = flake-utils.lib.mkApp {
-          drv = build-script;
+        typstPackagesCache = pkgs.stdenv.mkDerivation {
+          name = "typst-packages-cache";
+          src = typstPackagesSrc;
+          dontBuild = true;
+          installPhase = ''
+            mkdir -p "$out"
+            cp -LR --reflink=auto --no-preserve=mode -t "$out" "$src"/*
+          '';
         };
-        watch = flake-utils.lib.mkApp {
-          drv = watch-script;
-        };
-      };
 
-      devShells.default = typixLib.devShell {
-        inherit (commonArgs) fontPaths virtualPaths;
-        packages = [
-          # WARNING: Don't run `typst-build` directly, instead use `nix run .#build`
-          # See https://github.com/loqusion/typix/issues/2
-          # build-script
-          watch-script
-          # More packages can be added here, like typstfmt
-          # pkgs.typstfmt
-        ];
-      };
-    });
+        commonArgs = {
+          typstSource = "main.typ";
+
+          fontPaths = [
+            # Add paths to fonts here
+            # "${pkgs.roboto}/share/fonts/truetype"
+          ];
+
+          typstOpts = {
+            package-path = typstPackagesCache;
+          };
+
+          virtualPaths = [
+            # Add paths that must be locally accessible to typst here
+            # {
+            #   dest = "icons";
+            #   src = "${inputs.font-awesome}/svgs/regular";
+            # }
+          ];
+        };
+
+        # Compile a Typst project, *without* copying the result
+        # to the current directory
+        build-drv = typixLib.buildTypstProject (
+          commonArgs
+          // {
+            inherit src;
+          }
+        );
+
+        # Compile a Typst project, and then copy the result
+        # to the current directory
+        build-script = typixLib.buildTypstProjectLocal (
+          commonArgs
+          // {
+            inherit src;
+          }
+        );
+
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+      in
+      {
+        checks = {
+          inherit build-drv build-script watch-script;
+        };
+
+        packages.default = build-drv;
+
+        apps = rec {
+          default = watch;
+          build = flake-utils.lib.mkApp {
+            drv = build-script;
+          };
+          watch = flake-utils.lib.mkApp {
+            drv = watch-script;
+          };
+        };
+
+        formatter = treefmtEval.config.build.wrapper;
+
+        devShells.default = typixLib.devShell {
+          inherit (commonArgs) fontPaths virtualPaths;
+          packages = [
+            # WARNING: Don't run `typst-build` directly, instead use `nix run .#build`
+            # See https://github.com/loqusion/typix/issues/2
+            # build-script
+            watch-script
+            # More packages can be added here, like typstfmt
+            # pkgs.typstfmt
+          ];
+        };
+      }
+    );
 }
