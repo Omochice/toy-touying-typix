1830eb6bf01cc1d26b542ed017d192186220104b
Omochice <mochice.mls.ntl@gmail.com>
2024-12-10 22:22:49 +0900

    chore: add more formatters

--- treefmt.nix
+++ treefmt.nix
@@ -1,5 +1,17 @@
 { ... }:
 {
-  programs.nixfmt.enable = true;
-  programs.typstyle.enable = true;
+  programs = {
+    nixfmt.enable = true;
+    typstyle.enable = true;
+    mdformat.enable = true;
+    yamlfmt = {
+      enable = true;
+      settings = {
+        formatter = {
+          type = "basic";
+          retain_line_breaks_single = true;
+        };
+      };
+    };
+  };
 }
